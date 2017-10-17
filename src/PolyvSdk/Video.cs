47f44b212e89c78f97994b10ebf64e41d989cf60
刘金生 <abluesky@outlook.com>
2017-10-17 09:19:20 +0800

    Add Project

---
+++ src/PolyvSdk/Video.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace PolyvSdk
+{
+
+    [JsonObject(NamingStrategyType = typeof(SnakeCaseNamingStrategy))]
+    public class Video
+    {
+
+        public string SwfLink { get; set; }
+
+        public int SourceFileSize { get; set; }
+
+        public int Status { get; set; }
+        public string Tag { get; set; }
+        public int Seed { get; set; }
+        public int PlayerWidth { get; set; }
+        public List<long> FileSize { get; set; }
+        public string Duration { get; set; }
+        public string Title { get; set; }
+        public int Df { get; set; }
+        public string FirstImage { get; set; }
+        public int Times { get; set; }
+        public string Context { get; set; }
+        public string OriginalDefinition { get; set; }
+        public int PlayerHeight { get; set; }
+        public string Vid { get; set; }
+        public string Ptime { get; set; }
+        public long Cataid { get; set; }
+        
+    }
+}
---
+++ src/PolyvSdk/VideoManageClient.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace PolyvSdk
+{
+    /// <summary>
+    /// 用于管理视频的 客户端
+    /// </summary>
+    public class VideoManageClient:IDisposable
+    {
+        private HttpClient _httpClient = new HttpClient();
+        public HttpClient HttpClient
+        {
+            get => _httpClient ;
+            set => _httpClient = value;
+        }
+
+        public string ReadToken { get; set; }
+        public string WriteToken { get; set; }
+        public void Dispose()
+        {
+            _httpClient?.Dispose();
+        }
+
+        public async Task<IList<Video>> GetVideoListAsync(int page,int pageSize)
+        {
+            var json = await HttpClient.GetStringAsync($"http://v.polyv.net/uc/services/rest?method=getNewList&readtoken={ ReadToken }&pageNum={ page }&numPerPage={pageSize}");
+            return JsonConvert.DeserializeObject<List<Video>>(json);
+        }
+
+        public async Task<bool> DeleteVideo(string videoId)
+        {
+            var json = await HttpClient.GetStringAsync($"http://v.polyv.net/uc/services/rest?method=delVideoById&writetoken={ WriteToken }&vid={ videoId }");
+            var error = JObject.Parse(json).Value<int>("error");
+            if(error == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public async Task<Video> GetVideo(string videoId)
+        {
+            var json = await HttpClient.GetStringAsync($"http://v.polyv.net/uc/services/rest?method=getById&readtoken={ ReadToken }&vid={ videoId }");
+            return JsonConvert.DeserializeObject<Video>(json);
+        }
+
+    }
+}
